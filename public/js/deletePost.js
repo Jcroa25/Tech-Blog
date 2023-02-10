5bf72e495d959514d42e878c00dc05ae6c40384f
JuanCarlos Roa Jr <roajrjuancarlos@hotmail.com>
2023-02-09 23:40:01 -0500

    completed delete.js code

--- public/js/deletePost.js
+++ public/js/deletePost.js
@@ -1,17 +1,15 @@
-async function commentFormHandler(event) {
+async function deleteFormHandler(event) {
     event.preventDefault();
 
-const comment_text = document.querySelector('input[name="comment-body"]').value.trim();
-const post_id = window.location.toString().split('/')[
+const id = window.location.toString().split('/')[
  window.location.toString().split('/').length - 1
 ];
 
   if (comment_text) {
    const response = await fetch('/api/comments', {
-    method: 'POST',
+    method: 'DELETE',
      body: JSON.stringify({
-      post_id,
-      comment_text
+      post_id, id
      }),
      headers: {
       'Content-Type': 'application/json'
